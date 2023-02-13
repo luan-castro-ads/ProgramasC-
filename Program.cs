5ce8616b072002cae5978a7f5ddcf6e921f8b784
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-02-12 21:42:52 -0300

    Converter Celsius em Fahrenheit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*Faça um programa que lê uma temperatura em
+graus Celsius e apresenta-a convertida em graus
+Fahrenheit. A fórmula de conversão: F ←
+(9*C+160)/5*/
+
+double temperaturacelsius;
+double temperaturafahrenheit;
+
+Console.WriteLine("Informe uma temperatura em graus Celsius.");
+temperaturacelsius = Convert.ToDouble(Console.ReadLine());
+
+temperaturafahrenheit = ( 9 * temperaturacelsius + 160) / 5;
+
+Console.WriteLine("A temperatura informada corresponde a {0} graus fahrenheit.", temperaturafahrenheit);
