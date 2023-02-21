ac58bb73f278a5e3bbd30ec83603535842534bc8
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-02-21 08:36:07 -0300

    Exercício 4 - Número anterior e Sucessor
    Exercício 4

---
+++ Exercício_4/program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Faça um programa que leia um número inteiro
+e imprima o seu antecessor e sucessor.*/
+
+int numero;
+int numeroanterior;
+int numeroposterior;
+
+Console.WriteLine("Informe um numero inteiro.");
+numero = Convert.ToInt32(Console.ReadLine());
+
+numeroanterior = numero - 1;
+
+numeroposterior = numero + 1;
+
+Console.WriteLine("O numero anterior ao número informado é: {0}", numeroanterior);
+
+Console.WriteLine("O numero posterior ao número informado é: {0}", numeroposterior);
+
+
