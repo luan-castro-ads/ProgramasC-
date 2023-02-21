de4cf610530b01f5a5396d816a8e33e45a32d860
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-02-21 15:12:58 -0300

    Exercício 12 - volume caixa retangular
    Exercício 12

---
+++ Exercício_12/program.cs
@@ -0,0 +1,21 @@
+﻿/*Elaborar um programa que calcule e apresente o volume de uma 
+caixa retangular, por meio da fórmula: 
+volume = comprimento*largura*altura*/
+
+double comprimento;
+double largura;
+double altura;
+double volume;
+
+Console.WriteLine("Informe o comprimento da caixa em centímetros: ");
+comprimento = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe a largura da caixa em centímetros: ");
+largura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe a altura da caixa em centímetros: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+volume = comprimento * largura * altura;
+
+Console.WriteLine("O volume da caixa retangular é: {0} cm³.", volume);
