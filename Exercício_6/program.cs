db4e8eb91e935849cd4588309c0e756c2c1ae644
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-02-21 09:18:06 -0300

    Exercício 6 - Converter Segundos em horas, minutos e segundos
    Exercício 6 - Converter segundos

---
+++ Exercício_6/program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Construa um programa para ler um intervalo
+de tempo em segundos, converter para
+horas, minutos e segundos e imprimir o
+resultado.*/
+
+int quantidadesegundos;
+int horas;
+int minutos;
+int segundos;
+
+Console.WriteLine("Informe uma quantidade em segundos: ");
+quantidadesegundos = Convert.ToInt32(Console.ReadLine());
+
+horas = quantidadesegundos / 3600;
+minutos = (quantidadesegundos % 3600) / 60;
+segundos = quantidadesegundos - ((horas * 3600) + ( minutos * 60));
+
+Console.WriteLine("A quantidade de segundos informada corresponde a: {0} horas, {1} minutos e {2} segundos", horas, minutos, segundos);
+
