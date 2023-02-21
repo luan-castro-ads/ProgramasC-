d94e378fb625adb13517b2205a840b854314c64b
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-02-21 07:42:34 -0300

    Exercício 2 - Proporção Salarial
    Proporção Salarial

---
+++ Exercício_2/program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Faça um programa que lê um valor de salário
+mínimo e o salário de um funcionário. O programa
+deve calcular e imprimir quantos salários mínimos
+esse funcionário ganha. */
+
+double salariominimo;
+double salariofuncionario;
+double proporcaosalarial;
+
+Console.WriteLine("Informe o valor do salário minímo.");
+salariominimo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe o salário do funcionário.");
+salariofuncionario = Convert.ToDouble(Console.ReadLine());
+
+proporcaosalarial = salariofuncionario / salariominimo;
+
+Console.WriteLine("O funcionario recebe o equivalente a: {0:N2} salários minímos", proporcaosalarial);
+
