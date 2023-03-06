9a430ddaa5a2b8d4ad038fc1dbc168d5f4c5fae6
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-03-06 16:27:03 -0300

    Exercício 15 - Função para aplicar acréscimo no valor
    Exercício 15

---
+++ Exercício_15/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*Faça uma função que receba o valor de um produto 
+e um percentual de acréscimo. A função deve retornar 
+o valor do produto após a aplicação do acréscimo.*/
+
+static double jurosproduto (double preco, double acrescimo, double aumentopreco) {
+    
+    double precoatualizado;
+
+    precoatualizado = preco + aumentopreco;
+    return precoatualizado;
+}
+
+double preco;
+double acrescimo;
+double aumentopreco;
+double precoatualizado;
+
+
+Console.WriteLine("Informe o preço do produto: ");
+preco = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe o percentual de acréscimo do produto: ");
+acrescimo = Convert.ToDouble(Console.ReadLine());
+    if(acrescimo > 0) {
+        Console.WriteLine("Faixa de acréscimo no valor do produto aceita.");
+    } else (acrescimo < 0 && acrescimo >= - 100) {
+        Console.WriteLine("Ao aplicar esta faixa de acréscimo. Você estará dando um desconto ao invés de aumentar o preço do produto.");
+    }
+
+aumentopreco = (acrescimo / 100) * preco;
+
+precoatualizado = jurosproduto (preco, acrescimo, aumentopreco);
+
+Console.WriteLine("O preço do produto após a aplicação do acréscimo é: R$ {0:n2}", precoatualizado);
