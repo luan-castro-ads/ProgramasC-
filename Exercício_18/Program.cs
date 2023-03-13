7482deebaa168164918ff553386ee3cd76081d85
luan-castro-ads <luan.castro@viannasempre.com.br>
2023-03-13 15:44:19 -0300

    Exercício 18 - Procedimento que calcula duração de eventos em segundos
    Exercício 18

---
+++ Exercício_18/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Faça um procedimento que receba por parâmetro 
+o tempo de duração de um experimento expresso 
+em segundos e imprima na tela esse mesmo tempo 
+em horas, minutos e segundos.*/
+
+static void duracaoevento (int duracao) {
+
+    int horas;
+    int minutos;
+    int segundos;
+    
+    horas = duracao / 3600;
+
+    minutos = (duracao % 3600) / 60;
+
+    segundos = duracao - ((horas * 3600) + (minutos * 60));
+
+    Console.WriteLine("O evento informado durou: {0} horas, {1} minutos e {2} segundos.", horas, minutos, segundos);
+}
+
+int duracaosegundos; 
+
+Console.WriteLine("Informe a duração do evento em segundos: ");
+duracaosegundos = Convert.ToInt32(Console.ReadLine());
+
+duracaoevento(duracaosegundos);
+
+
+
+
